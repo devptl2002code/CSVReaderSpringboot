a59c516b25370173ce4ec04c9b2dade16da52886
devptl2002code <153890558+devptl2002code@users.noreply.github.com>
2024-03-05 02:36:21 +0530

    added initial files

---
+++ CSVDemo/CSVDemo/src/main/java/com/devptl/CSVDemo/CsvDemoApplication.java
@@ -0,0 +1,13 @@
+package com.devptl.CSVDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsvDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsvDemoApplication.class, args);
+	}
+
+}
